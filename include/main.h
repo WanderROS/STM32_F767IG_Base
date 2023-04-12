a672c40df1d5af0eaa2e51cfb5f3d1a9fc85efa7
wangsp14 <wangsp14@midea.com>
2023-04-12 20:30:08 +0800

    调试串口初始化成功。

---
+++ include/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+void SystemClock_Config(void);
+
+#endif /* __MAIN_H__ */
