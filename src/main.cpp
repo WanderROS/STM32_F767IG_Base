5190a2c63e74e9895b359baf445582ccafba7c49
wangsp14 <wangsp14@midea.com>
2023-04-14 14:56:34 +0800

    fatfs init & test ok.

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,26 @@ extern "C"
 #include <stdio.h>
 #include "bsp/bsp_debug_usart.h"
 #include "bsp/bsp_led.h"
-#include "bsp/bsp_qspi_flash.h"
 #include "bsp/bsp_sdram.h"
+#include "../lib/FATFS/ff.h"
+#include "../lib/FATFS/drivers/fatfs_flash_qspi.h"
+#include "../lib/FATFS/ff_gen_drv.h"
+
+    /**
+     ******************************************************************************
+     *                              定义变量
+     ******************************************************************************
+     */
+    char QSPIPath[4];            /* QSPI flash逻辑驱动器路径 */
+    FATFS fs;                    /* FatFs文件系统对象 */
+    FIL fnew;                    /* 文件对象 */
+    FRESULT res_flash;           /* 文件操作结果 */
+    UINT fnum;                   /* 文件成功读写数量 */
+    BYTE ReadBuffer[1024] = {0}; /* 读缓冲区 */
+    BYTE WriteBuffer[] =         /* 写缓冲区*/
+        "欢迎使用野火STM32 F767开发板 今天是个好日子，新建文件系统测试文件\r\n";
+    extern FATFS flash_fs;
+    extern Diskio_drvTypeDef QSPI_Driver;
 }
 #define FLASH_WriteAddress 0x00000
 #define FLASH_ReadAddress FLASH_WriteAddress
@@ -19,9 +37,6 @@ extern "C"
 uint8_t Rx_Buffer[100];
 int main()
 {
-    // 读取的 Flash ID 存储位置
-    __IO uint32_t DeviceID = 0;
-    __IO uint32_t FlashID = 0;
 
     /* 系统时钟初始化成216 MHz */
     SystemClock_Config();
@@ -30,27 +45,100 @@ int main()
     /* 初始化 LED */
     LED_GPIO_Config();
     cout << "STM32 F767IGT6 调试串口初始化成功！" << endl;
-    /* 16M 串行 flash W25Q128 初始化 */
-    QSPI_FLASH_Init();
-    /* 获取 Flash Device ID */
-    DeviceID = QSPI_FLASH_ReadDeviceID();
-    HAL_Delay(200);
-
-    /* 获取 SPI Flash ID */
-    FlashID = QSPI_FLASH_ReadID();
-    printf("\r\nFlashID is 0x%X,  Manufacturer Device ID is 0x%X\r\n", FlashID, DeviceID);
-    /* 检验 SPI Flash ID */
-    if (FlashID == sFLASH_ID)
+    printf("****** 这是一个SPI FLASH 文件系统实验 ******\r\n");
+    // 链接驱动器，创建盘符
+    FATFS_LinkDriver(&QSPI_Driver, QSPIPath);
+    // 在外部SPI Flash挂载文件系统，文件系统挂载时会对SPI设备初始化
+    res_flash = f_mount(&fs, "0:", 1);
+    /*----------------------- 格式化测试 ---------------------------*/
+    /* 如果没有文件系统就格式化创建创建文件系统 */
+    if (res_flash == FR_NO_FILESYSTEM)
     {
-        printf("\r\n检测到QSPI FLASH W25Q128 !\r\n");
-        /* 将刚刚写入的数据读出来放到接收缓冲区中 */
-        BSP_QSPI_Read(Rx_Buffer, FLASH_ReadAddress, 100);
-        printf("\r\n读出的数据为：\r\n%s", Rx_Buffer);
+        printf("》FLASH还没有文件系统，即将进行格式化...\r\n");
+        /* 格式化 */
+        res_flash = f_mkfs("0:", 0, 0);
+
+        if (res_flash == FR_OK)
+        {
+            printf("》FLASH已成功格式化文件系统。\r\n");
+            /* 格式化后，先取消挂载 */
+            res_flash = f_mount(NULL, "0:", 1);
+            /* 重新挂载	*/
+            res_flash = f_mount(&fs, "0:", 1);
+        }
+        else
+        {
+            printf("《《格式化失败。》》\r\n");
+            while (1)
+                ;
+        }
+    }
+    else if (res_flash != FR_OK)
+    {
+        printf("外部Flash挂载文件系统失败。(%d)\r\n", res_flash);
+        printf("可能原因:SPI Flash初始化不成功。\r\n");
+        while (1)
+            ;
     }
     else
     {
-        printf("\r\n获取不到 W25Q128 ID!\n\r");
+        printf("》文件系统挂载成功，可以进行读写测试\r\n");
     }
+    /*----------------------- 文件系统测试：写测试 -----------------------------*/
+    /* 打开文件，如果文件不存在则创建它 */
+    printf("\r\n****** 即将进行文件写入测试... ******\r\n");
+    res_flash = f_open(&fnew, "0:FatFs读写测试文件.txt", FA_CREATE_ALWAYS | FA_WRITE);
+    if (res_flash == FR_OK)
+    {
+        printf("》打开/创建FatFs读写测试文件.txt文件成功，向文件写入数据。\r\n");
+        /* 将指定存储区内容写入到文件内 */
+        res_flash = f_write(&fnew, WriteBuffer, sizeof(WriteBuffer), &fnum);
+        if (res_flash == FR_OK)
+        {
+            printf("》文件写入成功，写入字节数据：%d\n", fnum);
+            printf("》向文件写入的数据为：\r\n%s\r\n", WriteBuffer);
+        }
+        else
+        {
+            printf("！！文件写入失败：(%d)\n", res_flash);
+        }
+        /* 不再读写，关闭文件 */
+        f_close(&fnew);
+    }
+    else
+    {
+        printf("！！打开/创建文件失败。\r\n");
+    }
+    /*------------------- 文件系统测试：读测试 ------------------------------------*/
+    printf("****** 即将进行文件读取测试... ******\r\n");
+    res_flash = f_open(&fnew, "0:FatFs读写测试文件.txt", FA_OPEN_EXISTING | FA_READ);
+    if (res_flash == FR_OK)
+    {
+        printf("》打开文件成功。\r\n");
+        res_flash = f_read(&fnew, ReadBuffer, sizeof(ReadBuffer), &fnum);
+        if (res_flash == FR_OK)
+        {
+            printf("》文件读取成功,读到字节数据：%d\r\n", fnum);
+            printf("》读取得的文件数据为：\r\n%s \r\n", ReadBuffer);
+        }
+        else
+        {
+            printf("！！文件读取失败：(%d)\n", res_flash);
+        }
+    }
+    else
+    {
+        printf("！！打开文件失败。\r\n");
+    }
+    /* 不再读写，关闭文件 */
+    f_close(&fnew);
+
+    /*
+     *
+     * 不再使用文件系统，取消挂载文件系统
+     */
+    f_mount(NULL, "0:", 1);
+
     /*初始化SDRAM模块*/
     SDRAM_Init();
     SDRAM_Test();
